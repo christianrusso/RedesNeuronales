ddfdefaee0cf209d2add3bf4ec6f698ca0706f10
Hernan Bari <hernansoyrecapo@hotmail.com>
2016-09-09 15:02:42 -0300

    Corrige calculo error y otras cosas

--- TP1/mult_percep_ej1.py
+++ TP1/mult_percep_ej1.py
@@ -9,11 +9,12 @@ def cod(c):
 		return -1
 
 def main():
-	global X, Z, L, W, dW, eta, P, Y, S
+	global X, Z, L, W, dW, eta, P, Y, S, momentum
 	epsilon = 0.1
 	tau = 1000
-	eta = 0.1
+	eta = 0.01
 	p = 1
+	momentum = 0.55
 	
 	# XOR DE DOS VARIABLES 
 	#X = array([[-1,-1], [0,1], [1,0], [1,1], [0, 0]])
@@ -34,7 +35,7 @@ def main():
 			Z.append(z_i)
 	X = [x_i]+X
 	Z = [z_i]+Z
-	# print X
+	# print X 	#debug
 	media = mean(X, axis= 0) 
 	varianza = std(X, axis=0)
 	media_z = mean(Z, axis=0)
@@ -45,27 +46,28 @@ def main():
 		X[i] = (X[i] - media )/varianza
 
 	X = array(X)
-	# print X
+	# print X 	#debug
 	# print X[0] #debug
 	# print Z[0] #debug
 	
 	# CANT PATRONES
+	# -1 pq x esta indizado desde 1
 	P = len(X)-1
 	# CANT CAPAS
 	L = 3
 	# UNIDADES POR CAPA 
 	S = [1, shape(X)[1]]
-	S.extend([L*2 for x in range(2, L)])
+	S.extend([15 for x in range(2, L)])
 	S.append(shape(Z)[1])
 	# TAMANOS W, dW, Y
-	W = [random.uniform(-1/sqrt(S[j]),1/sqrt(S[j]), (S[j-1], S[j])) for j in range(0, L+1)]
+	W = [random.uniform(-sqrt(S[j]),sqrt(S[j]), (S[j-1], S[j])) for j in range(0, L+1)]
 	dW = [zeros((S[j-1], S[j])) for j in range(0, L+1)]
-	print dW
+	# print dW 		#debug
 	Y = [zeros((1, S[j]+1)) for j in range(0, L)]
 	for x in xrange(len(Y)):
 		Y[x][0][-1] = -1
 	Y.append([random.uniform(-1/sqrt(2),1/sqrt(2),(1,shape(Z)[1]))[newaxis]])
-	# print Y
+	# print Y 	# debug
 	# print L, S #debug
 	# print P  #debug
 	# print shape(X), shape(Z) #debug
@@ -79,12 +81,14 @@ def holdout(epsilon, tau, p):
 	e_v = 1
 	t = 0
 	v = int(p*P)
-	while(e_t>epsilon and t<tau):
-		print "epoch", t, "   error", e_t  #debug
-		# print X[:v+1] 
-		# print X[v+1:]
+	while(t<tau and e_t > epsilon):
+		print "epoch", t, "   e_training", e_t, "	e_validation", e_v
+		# print X[:v+1] 	#debug
+		# print X[v+1:] 	#debug
 		e_t = incremental(X[:v+1],Z[:v+1])
-		# e_v = testing(X[v:],Z[v:])
+		##### CAMBIAR CUANDO SE ARREGLE OUTPUT Y MAIN
+		# deberia ser v+1, pero si es vacio revienta
+		e_v = testing(X[v:],Z[v:])
 		t += 1
 	return e_t, e_v, t 
 
@@ -96,7 +100,7 @@ def incremental(X, Z):
 		activation(X[h])
 		e += correction(Z[h])
 		adaptation()
-	return e 	
+	return e/len(X) 	
 
 def activation(X_h):
 	global Y
@@ -111,7 +115,7 @@ def activation(X_h):
 def correction(Z_h):
 	global dW
 	E = Z_h - Y[L]
-	e = square(linalg.norm(E))
+	e = (E**2).sum()
 	for j in range(L, 1, -1):   
 		# print "L:", L, "j:", j
 		# print "j: ", j #debug 
@@ -123,10 +127,10 @@ def correction(Z_h):
 		# print "dW[j]: ", dW[j] #debug 
 		# print "D: ", D #debug 
 
-		dW[j] += (eta*dot(transpose(Y[j-1]), D))
+		dW[j] = ((eta*dot(transpose(Y[j-1]), D)) + momentum*dW[j])
 		# print dW[j] #debug 
 		E = dot(D, transpose(W[j]))
-	return e 
+	return e
 
 def adaptation():
 	global W
@@ -135,7 +139,7 @@ def adaptation():
 		W[j] += dW[j]
 		#print "j: ", j #debug 
 		#print "W[j]: ", W[j] #debug 
-		dW[j] = 0 
+		#dW[j] = 0 
 		#print "W[j]: ", W[j] #debug 
 
 def testing(X, Z):
@@ -143,12 +147,13 @@ def testing(X, Z):
 	for (X_h, Z_h) in zip(X, Z):
 		E = activation(X_h)-Z_h
 		e += square(linalg.norm(E))
-	return e 
+	return e/len(X) 
 
 def bipolar(vector, derivative=False):
 	B = 1
 	if derivative:
-		return B * (1 -bipolar(vector)*bipolar(vector))
+		return B * (1 - bipolar(vector)**2)
+		#return 1-vector**2
 	else:
 		return tanh(B*vector)
 
