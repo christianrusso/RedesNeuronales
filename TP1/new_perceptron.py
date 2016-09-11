6f9f3ef88ee6d072c64d639ca01a3c42454551e9
Hernan Bari <hernansoyrecapo@hotmail.com>
2016-09-11 19:31:00 -0300

    Código: arregla indexación desde 0 y otros errores

--- TP1/new_perceptron.py
+++ TP1/new_perceptron.py
@@ -28,9 +28,6 @@ class perceptron_Multiple:
 				z_i = map(self.cod, r[0])
 				self.X.append(x_i)
 				self.Z.append(z_i)
-		# basura en la primera pos pq esta indizado dsese 1
-		insert(self.X, 0, [0])
-		insert(self.Z, 0, [0])
 		self.normalizar_input()
 
 	def load_dataset_2(self,dataset):
@@ -45,9 +42,6 @@ class perceptron_Multiple:
 				z_i = map(float, r[-2:])
 				self.X.append(x_i)
 				self.Z.append(z_i)
-		# basura en la primera pos pq esta indizado dsese 1
-		insert(self.X, 0, [0])
-		insert(self.Z, 0, [0])
 		self.normalizar_input()
 		self.normalizar_output()
 	
@@ -82,19 +76,19 @@ class perceptron_Multiple:
 			self.Z[i] = (self.Z[i] - media_z)/varianza_z
 
 	def train(self):
-		self.P = len(self.X)-1
-		self.S = [1, shape(self.X)[1]]
+		self.P = len(self.X)
+		self.S = [shape(self.X)[1]]
 		#S.extend([15 for x in range(2, L)])
 		self.S.extend(self.UnitsXCapa)
 		self.S.append(shape(self.Z)[1])
 		# TAMANOS W, dW, Y
-		self.W = [random.uniform(-sqrt(self.S[j]),sqrt(self.S[j]), (self.S[j-1]+1, self.S[j])) for j in range(0, self.L+1)]
-		self.dW = [zeros((self.S[j-1]+1, self.S[j])) for j in range(0, self.L+1)]
-		self.Y = [zeros((1, self.S[j]+1)) for j in range(0, self.L)]
+		self.W = [random.uniform(-sqrt(self.S[j]),sqrt(self.S[j]), (self.S[j-1]+1, self.S[j])) for j in range(self.L)]
+		self.dW = [zeros((self.S[j-1]+1, self.S[j])) for j in range(self.L)]
+		self.Y = [zeros((1, self.S[j]+1)) for j in range(self.L-1)]
 		self.Y.append([zeros((1,shape(self.Z)[1]))])
 		t,error_v_hist,error_t_hist = self.holdout(self.epsilon, self.tau, self.p)
 		print error_t_hist[-1],error_v_hist[-1] ,t 
-		return error_v_hist,error_t_hist,t
+		return error_t_hist,error_v_hist,t
 
 	def holdout(self,epsilon, tau, p):
 		e_t = 1
@@ -105,7 +99,7 @@ class perceptron_Multiple:
 		error_t_hist=[]
 		while(t<self.tau and e_t > self.epsilon):
 			
-			e_t = self.incremental(self.X[:v+1],self.Z[:v+1])
+			e_t = self.incremental(self.X[:v],self.Z[:v])
 			e_v = self.testing(self.X[v:],self.Z[v:])
 			error_v_hist.append(e_v)
 			error_t_hist.append(e_t)
@@ -116,25 +110,25 @@ class perceptron_Multiple:
 
 	def incremental(self,X, Z):
 		e = 0
-		for h in range(1, self.P+1): 
+		for h in range(self.P): 
 			self.activation(X[h])
 			e += self.correction(Z[h])
 			self.adaptation()
-		return e/len(X) 	
+		return e/(len(X) if len(X) != 0 else 1) 	
 
 	def activation(self,X_h):
-		self.Y[1] = append(X_h, [-1])[newaxis]
-		for j in range(2, self.L+1):
-			if j == self.L:
+		self.Y[0] = append(X_h, [-1])[newaxis]
+		for j in range(1, self.L):
+			if j == self.L-1:
 				self.Y[j] = self.funcActivacion(dot(self.Y[j-1], self.W[j]))
 			else:
 				self.Y[j] = append(self.funcActivacion(dot(self.Y[j-1], self.W[j])), [-1])[newaxis]
-		return self.Y[self.L]
+		return self.Y[-1]
 
 	def correction(self,Z_h):
-		E = Z_h - self.Y[self.L]
+		E = Z_h - self.Y[-1]
 		e = (E**2).sum()
-		for j in range(self.L, 1, -1):   
+		for j in range(self.L-1, 0, -1):   
 			D = E*self.funcActivacion(dot(self.Y[j-1], self.W[j]), True)
 			self.dW[j] = ((self.eta*dot(transpose(self.Y[j-1]), D)) + self.momentum*self.dW[j])
 			# El error no tiene sentido q tenga el -1 del final
@@ -142,12 +136,12 @@ class perceptron_Multiple:
 		return e
 
 	def adaptation(self):
-		for j in range(2, self.L+1):
+		for j in range(1, self.L):
 			self.W[j] += self.dW[j]
 
 	def testing(self,X, Z):
 		e = 0
-		for (X_h, Z_h) in zip(self.X, self.Z):
+		for (X_h, Z_h) in zip(X, Z):
 			E = self.activation(X_h)-Z_h
-			e += square(linalg.norm(E))
-		return e/len(self.X) 
+			e += (E**2).sum()
+		return e/(len(X) if len(X) != 0 else 1) 
