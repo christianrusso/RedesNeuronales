e68ed308425e51afcc25cc2791e57b9b9b7c955a
Hernan Bari <hernansoyrecapo@hotmail.com>
2016-10-10 21:22:42 -0300

    Código: cambia código esqueleto ej 2 a versión más clara

--- TP2/hebbian.py
+++ TP2/hebbian.py
@@ -35,3 +35,54 @@ class red_hebbiana:
                         dw[i][j] = learning_rate * y[j] * (x[i] - xe)
 
                 self.weights += dw
+
+
+
+###########################
+# EN TEORIA LO DE ABAJO ES LO QUE NOS DIERON TODAVIA
+# ES PARA OPTIMIZAR, ASI QUE NO DEBERIA SER NECESARIO
+
+# class GHANeuralNetwork():
+
+#     def __init__(self, ninputs, noutputs, sigma0, alfa):
+
+#         self.sigma0 = sigma0
+#         self.alfa = alfa
+
+#         self.ninputs = ninputs
+#         self.noutputs = noutputs
+#         self.weights = np.random.uniform(-0.5, 0.5, (noutputs, ninputs))
+
+#     def ninputs(self):
+#         return self.ninputs
+
+#     def noutputs(self):
+#         return self.noutputs
+
+#     def weights(self):
+#         return self.weights
+
+#     def activate(self, input):
+
+#         x = np.array([input]).T
+#         y = np.dot(self.weights, x)
+#         return y.T[0]
+
+#     def train(self, dataset, epochs, callback=None):
+
+#         for t in range(1, epochs + 1):
+
+#             sigma = self.sigma0 * (t ** -self.alfa)
+#             tdw = 0
+
+#             for x in dataset:
+
+#                 x = np.array([x]).T
+#                 y = np.dot(self.weights, x)
+
+#                 dw = sigma * ( np.dot(y, x.T) - np.dot(np.tril(np.dot(y, y.T)), self.weights) )
+#                 self.weights += dw
+#                 tdw += dw ** 2
+
+#             if callback: callback(self, t, tdw.sum() / len(dataset))
+
--- TP2/som.py
+++ TP2/som.py
@@ -1,115 +1,55 @@
 from math import exp, sqrt
 import numpy as np
 
-def norma2(vec):
-	return sqrt(np.dot(vec, vec.T))
+class som():
 
+    def __init__(self, ninputs, output_size, learning_rate, sigma):
 
-debug = False
-info = False
-conteo = True
+        self.learning_rate = learning_rate
+        self.sigma = sigma
 
-def normalizar(x):
-	norma = norma2(x)
-	if norma > 0:
-		return x/norma
-	else:
-		return x
-		
-def informar(texto, nivel):
-	if nivel:
-		print texto
+        self.ninputs = ninputs
+        self.output_size = output_size
+        self.noutputs = output_size[0] * output_size[1]
 
-		
-class SOM:
-	def __init__(self, m1, m2, nEntrada, sigma=1.0, lrate=0.5):
-		self.M1 = m1
-		self.M2 = m2
-		self.M = m1*m2
-		self.N = nEntrada
-		self.sigma = sigma
-		self.lrate = lrate
-		self.vecindadFilas = np.arange(self.M1)
-		self.vecindadColumnas = np.arange(self.M2)
-		
-		# La matriz W la implementaron como un unico vector
-		# Creo q al pedo, pq después transforman a las dimensiones tipo matriz
-		# No lo cambio ahora para no romper todo
-		# Esto es horrible
-		np.random.seed(randomSeed)		
-		self.W = np.random.rand(nEntrada, self.M)
-		self.W = np.array([normalizar(v) for v in self.W])
-		
-		
-		self.lrateInicial = lrate
-		self.sigmaInicial = sigma
-		self.epoch = 1 #Epocas completas
-		self.t = 1 #Iteraciones de entrenamiento
+        self.neigx = np.arange(output_size[0])
+        self.neigy = np.arange(output_size[1])
 
-	def pIndexAlMapa(self, index):
-		return (index / self.M2, index % self.M2)
-		
-	def pIndexDesdeMapa(self, x, y):
-		return x*self.M2 + y%self.M2
-		
-	def matrizSigmaCentrada(self, puntoCentro):
-		# Devuelve la matriz de factores sigma (aplanada)
-		factorSigma = 2.0*(self.sigma**2.0)
-		# Pienso (yo hernan) que esto está feo, pero yafue
-		filas = np.exp(-np.power(self.vecindadFilas-puntoCentro[0],2)/factorSigma)
-		columnas = np.exp(-np.power(self.vecindadColumnas-puntoCentro[1],2)/factorSigma)
-		
-		matriz = np.outer(filas, columnas)
-		return matriz.reshape(1, self.M)
-		
-	def activar(self, X):
-		# Devuelve el pIndex ganador (jStar)
-		
-		X = X.reshape(1, self.N)
-		XtMenosW = X.T - self.W
-		
-		yTilde = [norma2(XtMenosW[:, col]) for col in range(self.M)]
-		jStar = np.argmin(yTilde)
-		
-		return jStar
-	
-	def corregir(self, jStar):
-		# Toma un ganador y corrije los pesos.
-		D = self.matrizSigmaCentrada(self.pIndexAlMapa(jStar))
-		XtMenosW = X.T - self.W
-		
-		varW = self.lrate * np.multiply(D, XtMenosW)
-		self.W += varW
-		
-	def actualizarParametrosAdaptativos(self):
-		self.t += 1.0
-		#self.lrate = 1.0 / self.epoch**2
-		#self.sigma = self.M2 / (2.0 * (self.epoch ** 3))
-		self.lrate = self.lrateInicial / self.epoch
-		self.sigma = self.sigmaInicial / self.epoch
-		# NO SE PQ SE USABAN LOS DE ARRIBA, SEGUN MI CARPETA:
-		# self.lrate = 1/sqrt(self.epoch)
-		# self.sigma = self.M2 / (2.0 * (self.epoch ** (1/3)))
-		
-	def trainEpoch(self, X):
-		jStar = self.activar(X)
-		self.corregir(jStar)
-		self.actualizarParametrosAdaptativos()
-		return self.pIndexAlMapa(jStar)
-		
-	def trainRandom(self, Xs, maxEpoch):
+        self.weights = np.random.uniform(-0.5, 0.5, (self.ninputs, self.noutputs))
 
-		while self.epoch <= maxEpoch:
-			# Agarro la entrada shuffleada
-			indexes = np.arange(len(Xs))
-			np.random.shuffle(indexes)
-			print self.epoch
-			for index in indexes:
-				resMapeo = self.trainEpoch(Xs[index])
-	
-			self.epoch += 1
-			
-	def mapear(self, X):
-		res = self.pIndexAlMapa(self.activar(X))
-		return res
-	
+    def activate(self, input):
+
+        y = np.linalg.norm(self.weights - np.array([input]).transpose(), None, 0)
+        r = np.zeros_like(y)
+        r[y.argmin()] = 1
+
+        return r.reshape(self.output_size);
+
+    def gaussian(self, c, sigma):
+
+        d = 2 * np.pi * sigma * sigma
+        ax = np.exp(-np.power(self.neigx - c[0], 2) / d)
+        ay = np.exp(-np.power(self.neigy - c[1], 2)/ d)
+        return np.outer(ax, ay)
+
+    def correction(self, input, learning_rate, sigma):
+
+        y = self.activate(input)
+        p = np.unravel_index(y.argmax(), y.shape)
+        d = self.gaussian(p, sigma)
+        dw = learning_rate * (np.array([input]).transpose() - self.weights) * d.flatten()
+        self.weights += dw
+
+        return dw
+
+    def train(self, dataset, epochs, callback=None):
+
+        for t in range(1, epochs + 1):
+            eta = t ** (- self.learning_rate)
+            sigma = t ** (- self.sigma)
+
+            tdw = np.zeros((self.ninputs, self.noutputs))
+            for x in dataset:
+                tdw += self.correction(x, eta, sigma) ** 2
+
+            
