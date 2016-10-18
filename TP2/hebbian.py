016cc607e2d2fc24d13ad397f8658e58b8c8f483
Hernan Bari <hernansoyrecapo@hotmail.com>
2016-10-18 17:02:19 -0300

    Código: agrega algoritmos matriciales y corrige otros errores

--- TP2/hebbian.py
+++ TP2/hebbian.py
@@ -2,20 +2,20 @@ import numpy as np
 
 class red_hebbiana:
 
-    def __init__(self, ninputs, noutputs, learning_rate=None, use_oja=None):
+    def __init__(self, ninputs, noutputs, learning_rate=None, use_oja=True, optimized=True):
 
         self.learning_rate = learning_rate
         self.use_oja = use_oja
+        self.optimized = optimized
 
         self.ninputs = ninputs
         self.noutputs = noutputs
         self.weights = np.random.uniform(-0.5, 0.5, (ninputs, noutputs))
 
-    def activate(self, input):
+    def activate(self, x):
 
-        x = np.array([input])
         y = np.dot(x, self.weights)
-        return y[0]
+        return y
 
     def train(self, dataset, epochs):
         # TODO: En vez de epochs, agregar ortogonalidad como criterio de parada
@@ -23,16 +23,25 @@ class red_hebbiana:
             learning_rate = self.learning_rate or (0.5 / e)
 
             for x in dataset:
-                y = self.activate(x)
-                dw = np.zeros((self.ninputs, self.noutputs), dtype=float)
+                y = np.dot(x,self.weights)
+                if self.optimized:
+                    if self.use_oja:
+                        x_mono = np.dot(y, self.weights.T)
+                        dw = learning_rate * np.dot((x - x_mono).T, y)
+                    else:
+                        U = np.triu(np.ones(self.noutputs))
+                        x_mono = np.dot(self.weights, np.multiply(y.T, U))
+                        dw = learning_rate * np.multiply((x.T - x_mono),y)
+                else:
+                    dw = np.zeros((self.ninputs, self.noutputs), dtype=float)
 
-                for j in range(0, self.noutputs):
-                    for i in range(0, self.ninputs):
-                        xe = 0
-                        for k in range(0, self.use_oja and self.noutputs or (j + 1)):
-                            xe += y[k] * self.weights[i][k]
+                    for j in range(0, self.noutputs):
+                        for i in range(0, self.ninputs):
+                            x_mono = 0
+                            for k in range(0, self.use_oja and self.noutputs or (j + 1)):
+                                x_mono += y[k] * self.weights[i][k]
 
-                        dw[i][j] = learning_rate * y[j] * (x[i] - xe)
+                            dw[i][j] = learning_rate * (x[i] - x_mono)* y[j] 
 
                 self.weights += dw
             print "epoca: "+str(e)
@@ -42,7 +51,7 @@ class red_hebbiana:
         # TODO: En vez de epochs, agregar ortogonalidad como criterio de parada
         y=[]
         for x in dataset:
-            y.append([x[0]] + self.activate(x[1:]).tolist())
+            y.append(self.activate(np.array(x[1:]).reshape((1, 856))))
         return y
 
 
